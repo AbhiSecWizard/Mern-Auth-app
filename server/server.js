15083bf459eefc200aaa4c01c42bbadcf6740234
Abhishek yadav <studyandrotech@gmail.com>
2026-04-03 01:59:35 +0530

    env clean

--- server/server.js
+++ server/server.js
@@ -6,9 +6,9 @@ const cookieParser = require("cookie-parser")
 const connectDB = require("./config/mongodb")
 const userRouter = require("./routes/userRouter")
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 5000
 
-const allowedOrigins =["http://localhost:5173"]
+const allowedOrigins =["http://localhost:5174"]
 
 app.use(express.json())
 app.use(cookieParser())
