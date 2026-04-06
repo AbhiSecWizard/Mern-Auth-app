66084a7c7c1a11a28521b950337b7f9909abd83a
Abhishek yadav <studyandrotech@gmail.com>
2026-04-06 11:44:51 +0530

    Project Completed

--- server/server.js
+++ server/server.js
@@ -5,33 +5,28 @@ const authRouter = require("./routes/authRouter")
 const cookieParser = require("cookie-parser")
 const connectDB = require("./config/mongodb")
 const userRouter = require("./routes/userRouter")
+
 const path = require("path")
+
 const app = express()
+
 const port = process.env.PORT || 5000
 const allowedOrigins =["http://localhost:5173"]
-
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({origin:allowedOrigins,
               credentials:true}))
               connectDB()
-
-// API end Points
-
+// API end Points       
 app.use("/api/auth",authRouter)
 app.use("/api/user",userRouter)
 app.use(express.static(path.join(__dirname, "../client/dist")));
-
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
-
-
-
 app.get("/",(req,res)=>{
     res.send("API WORKING")
 })
-
 app.listen(port,()=>{
     console.log(`server is running on PORT : ${port}`)
 })
