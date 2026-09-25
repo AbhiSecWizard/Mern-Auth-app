33606e38bced4d43fd8063ac7efc5aa19fdda758
Abhishek Yadav <studyandrotech@gmail.com>
2026-09-25 06:35:11 +0530

    fix userAuth filename

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,20 +1,22 @@
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-      host:'smtp-relay.brevo.com',
-      port:587,
-      auth:{
-        user:process.env.SMTP_USER,
-        pass:process.env.SMTP_PASS
-      }
-})
+    host: "smtp-relay.brevo.com",
+    port: 2525,
+    secure: false,
 
-module.exports = transporter
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+    }
+});
 
+transporter.verify((error, success) => {
+    if (error) {
+        console.log("SMTP ERROR:", error);
+    } else {
+        console.log("SMTP SERVER READY");
+    }
+});
 
-
-
-
-
-
-
+module.exports = transporter;
